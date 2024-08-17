dd9811867beab851ee4b0df144db9eb7b4f61603
yuri-anjos <yuri.dos.4njos@gmail.com>
2024-08-16 22:46:31 -0300

    feature: project init

---
+++ src/main/java/br/com/syonet/desafiotecnicosyonet/DesafioTecnicoSyonetApplication.java
@@ -0,0 +1,13 @@
+package br.com.syonet.desafiotecnicosyonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoSyonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoSyonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/syonet/desafiotecnicosyonet/DesafioTecnicoSyonetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.syonet.desafiotecnicosyonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoSyonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
